46108ef94b5c0076f9ef6703f8789399e0ed7a49
Marcus Mu <chunkhang@gmail.com>
2018-11-07 11:36:24 +0800

    Add numbers exercise

---
+++ chapter-9/numbers.js
@@ -0,0 +1,14 @@
+const number = /^[+-]?(\d+|\.\d+|\d+\.(\d+e?\d+)?|\d+e[+-]\d+)$/i;
+
+for (const str of ['1', '-1', '+15', '1.55', '.5', '5.',
+                   '1.3e2', '1E-4', '1e+12']) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (const str of ['1a', '+-1', '1.2.3', '1+1', '1e4.5',
+                   '.5.', '1f5', '.']) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
